b1e199185f80df897eae348ee8127ca2b9ef0422
Sindre Sorhus <sindresorhus@gmail.com>
2017-12-19 17:09:36 +0100

    Improve benchmark

--- bench/bench.js
+++ bench/bench.js
@@ -1,30 +1,46 @@
+/* globals suite set bench */
 'use strict';
-
 const onChange = require('..');
 
-let i = 0;
 const save = object => {
-	console.log('Object changed:', object, ++i);
+	let i = object.a;
+	while (i < 1000) {
+		i++;
+	}
 };
 
 suite('onChange', () => {
-	set('mintime', 1000);
+	set('mintime', 5000);
 
-	bench('onChange', () => {
-		const foo = onChange({a: 0, b: 0}, () => save(foo));
+	bench('on-change', () => {
+		const foo = onChange({
+			a: 0,
+			b: 0,
+			c: 0,
+			d: 0
+		}, () => save(foo));
+
 		foo.a = 1;
 		foo.b = 2;
+		foo.c = 3;
+		foo.d = 4;
 	});
 
-	bench('save', () => {
+	bench('native', () => {
 		const foo = {
 			a: 0,
-			b: 0
+			b: 0,
+			c: 0,
+			d: 0
 		};
 
 		foo.a = 1;
 		save(foo);
 		foo.b = 2;
+		save(foo);
+		foo.c = 3;
+		save(foo);
+		foo.d = 4;
 		save(foo);
 	});
 });
