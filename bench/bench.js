caa29f63ad78108a9006eeb82774088da80822bb
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-01-15 01:45:19 +0100

    feat: include array bench

--- bench/bench.js
+++ bench/bench.js
@@ -2,8 +2,8 @@
 'use strict';
 const onChange = require('..');
 
-const save = object => {
-	let i = object.a;
+const save = (object, initial = 0) => {
+	let i = object[initial];
 	while (i < 1000) {
 		i++;
 	}
@@ -18,7 +18,7 @@ suite('onChange', () => {
 			b: 0,
 			c: 0,
 			d: 0
-		}, () => save(foo));
+		}, () => save(foo, 'a'));
 
 		foo.a = 1;
 		foo.b = 2;
@@ -35,12 +35,38 @@ suite('onChange', () => {
 		};
 
 		foo.a = 1;
-		save(foo);
+		save(foo, 'a');
 		foo.b = 2;
-		save(foo);
+		save(foo, 'a');
 		foo.c = 3;
-		save(foo);
+		save(foo, 'a');
 		foo.d = 4;
+		save(foo, 'a');
+	});
+});
+
+suite('bench with an array too', () => {
+	set('mintime', 5000);
+
+	bench('on-change', () => {
+		const foo = onChange([0, 0, 0, 0], () => save(foo));
+
+		foo[0] = 1;
+		foo[1] = 2;
+		foo[2] = 3;
+		foo[3] = 4;
+	});
+
+	bench('native', () => {
+		const foo = [0, 0, 0, 0];
+
+		foo[0] = 1;
+		save(foo);
+		foo[1] = 2;
+		save(foo);
+		foo[2] = 3;
+		save(foo);
+		foo[3] = 4;
 		save(foo);
 	});
 });
