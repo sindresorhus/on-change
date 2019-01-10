edbd926c2b7b509590e1db2a50643b5c45adc20c
Kris J. Pruden <kpruden@users.noreply.github.com>
2019-01-10 03:09:52 -0800

    Preserve proxy invariants (#16)
    Preserve invariants specified in https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/handler/get#Invariants
    
    Based on #15 and #8.
    
    Also add full support for accessor properties.

--- index.js
+++ index.js
@@ -1,50 +1,107 @@
 'use strict';
 
-const blacklist = [
-	'sort',
-	'reverse',
-	'splice',
-	'pop',
-	'unshift',
-	'shift',
-	'push'
-];
+function isPrimitive(value) {
+	return value === null || (typeof value !== 'object' && typeof value !== 'function');
+}
+
+const proxyTarget = Symbol('ProxyTarget');
 
 module.exports = (object, onChange) => {
-	let isBlocked = false;
+	let inApply = false;
+	let changed = false;
+	const propCache = new WeakMap();
+
+	function handleChange() {
+		if (!inApply) {
+			onChange();
+		} else if (!changed) {
+			changed = true;
+		}
+	}
+
+	function getOwnPropertyDescriptor(target, property) {
+		if (!propCache.has(target)) {
+			propCache.set(target, new Map());
+		}
+		const props = propCache.get(target);
+		if (props.has(property)) {
+			return props.get(property);
+		}
+		const prop = Reflect.getOwnPropertyDescriptor(target, property);
+		props.set(property, prop);
+		return prop;
+	}
+
+	function invalidateCachedDescriptor(target, property) {
+		if (!propCache.has(target)) {
+			return;
+		}
+		const props = propCache.get(target);
+		props.delete(property);
+	}
 
 	const handler = {
 		get(target, property, receiver) {
-			try {
-				return new Proxy(target[property], handler);
-			} catch (_) {
-				return Reflect.get(target, property, receiver);
+			if (property === proxyTarget) {
+				return target;
 			}
+
+			const value = Reflect.get(target, property, receiver);
+			if (isPrimitive(value)) {
+				return value;
+			}
+
+			// Preserve invariants
+			const descriptor = getOwnPropertyDescriptor(target, property);
+			if (descriptor && !descriptor.configurable) {
+				if (descriptor.set && !descriptor.get) {
+					return undefined;
+				}
+				if (descriptor.writable === false) {
+					return value;
+				}
+			}
+
+			return new Proxy(value, handler);
+		},
+		set(target, property, value, receiver) {
+			if (value && value[proxyTarget] !== undefined) {
+				value = value[proxyTarget];
+			}
+			const previous = Reflect.get(target, property, value, receiver);
+			const result = Reflect.set(target, property, value);
+
+			if (previous !== value) {
+				handleChange();
+			}
+
+			return result;
 		},
 		defineProperty(target, property, descriptor) {
 			const result = Reflect.defineProperty(target, property, descriptor);
+			invalidateCachedDescriptor(target, property);
 
-			if (!isBlocked) {
-				onChange();
-			}
+			handleChange();
 
 			return result;
 		},
 		deleteProperty(target, property) {
 			const result = Reflect.deleteProperty(target, property);
+			invalidateCachedDescriptor(target, property);
 
-			if (!isBlocked) {
-				onChange();
-			}
+			handleChange();
 
 			return result;
 		},
 		apply(target, thisArg, argumentsList) {
-			if (blacklist.includes(target.name)) {
-				isBlocked = true;
+			if (!inApply) {
+				inApply = true;
 				const result = Reflect.apply(target, thisArg, argumentsList);
-				onChange();
-				isBlocked = false;
+				if (changed) {
+					onChange();
+				}
+				inApply = false;
+				changed = false;
 				return result;
 			}
 
--- test.js
+++ test.js
@@ -30,6 +30,7 @@ test('main', t => {
 	t.is(callCount, 3);
 
 	delete object.foo;
+	t.is(object.foo, undefined);
 	t.is(callCount, 4);
 
 	object.bar.a.b = 1;
@@ -39,6 +40,24 @@ test('main', t => {
 	object.bar.a.c[2] = 5;
 	t.is(object.bar.a.c[2], 5);
 	t.is(callCount, 6);
+
+	object.bar.a.c[2] = 5;
+	t.is(callCount, 6);
+
+	// Unwrap proxies on assignment
+	const prev = fixture.bar.a;
+	object.bar.a = object.bar.a; // eslint-disable-line no-self-assign
+	t.is(fixture.bar.a, prev);
+
+	// Support null assignment
+	object.bar.a.c[2] = null;
+	t.is(object.bar.a.c[2], null);
+	t.is(callCount, 7);
+
+	// Support undefined assignment
+	object.bar.a.c[2] = undefined;
+	t.is(object.bar.a.c[2], undefined);
+	t.is(callCount, 8);
 });
 
 test('works with an array too', t => {
@@ -49,37 +68,81 @@ test('works with an array too', t => {
 	const array = onChange(fixture, () => {
 		callCount++;
 	});
-
 	array[0] = 'a';
-	t.deepEqual(array, ['a', 2, {a: false}]);
+	t.deepEqual(fixture, ['a', 2, {a: false}]);
+	t.deepEqual(array, fixture);
 	t.is(callCount, 1);
 
 	array[2].a = true;
+	t.deepEqual(fixture, ['a', 2, {a: true}]);
+	t.deepEqual(array, fixture);
 	t.is(callCount, 2);
 
 	array.sort();
+	t.deepEqual(fixture, [2, {a: true}, 'a']);
+	t.deepEqual(array, fixture);
 	t.is(callCount, 3);
 
 	array.pop();
+	t.deepEqual(fixture, [2, {a: true}]);
+	t.deepEqual(array, fixture);
 	t.is(callCount, 4);
 
 	array[2] = false;
+	t.deepEqual(fixture, [2, {a: true}, false]);
+	t.deepEqual(array, fixture);
 	t.is(callCount, 5);
 
 	array.reverse();
+	t.deepEqual(fixture, [false, {a: true}, 2]);
+	t.deepEqual(array, fixture);
 	t.is(callCount, 6);
 
 	array.reverse();
+	t.deepEqual(fixture, [2, {a: true}, false]);
+	t.deepEqual(array, fixture);
 	t.is(callCount, 7);
+
+	array.splice(1, 1, 'a', 'b');
+	t.deepEqual(fixture, [2, 'a', 'b', false]);
+	t.deepEqual(array, fixture);
+	t.is(callCount, 8);
 });
 
-// https://github.com/sindresorhus/on-change/issues/14
-test.failing('Array#splice works', t => {
-	const array = onChange([1, 2, 3], () => {});
-
-	t.notThrows(() => {
-		array.splice(0, 1);
+test('invariants', t => {
+	const fixture = {};
+	Object.defineProperty(fixture, 'nonWritable', {
+		configurable: false,
+		writable: false,
+		value: {a: true}
 	});
+	// eslint-disable-next-line accessor-pairs
+	Object.defineProperty(fixture, 'nonReadable', {
+		configurable: false,
+		set: () => {} // No-Op setter
+	});
+	Object.defineProperty(fixture, 'useAccessor', {
+		configurable: false,
+		set(val) {
+			this._useAccessor = val;
+		},
+		get() {
+			return this._useAccessor;
+		}
+	});
+
+	let callCount = 0;
+
+	const proxy = onChange(fixture, () => {
+		callCount++;
+	});
+
+	t.is(proxy.nonWritable, fixture.nonWritable);
+	t.is(proxy.nonReadable, undefined);
+
+	proxy.useAccessor = 10;
+	t.is(proxy.useAccessor, 10);
+	t.is(callCount, 1);
 });
 
 test.cb('the change handler is called after the change is done', t => {
