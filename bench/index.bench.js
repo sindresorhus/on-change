cad2a29938c5d6280f81cc0ebec82a860d05cbf4
Joël Charles <joel.charles91@gmail.com>
2024-05-16 14:45:35 +0200

    Migrate benchmark to tinybench: entry point

---
+++ bench/index.bench.js
@@ -0,0 +1,6 @@
+await import("./main.bench.js");
+await import("./array.bench.js");
+await import("./cache.bench.js");
+await import("./is-builtin.bench.js");
+await import("./path.bench.js");
+await import("./smart-clone.bench.js");
